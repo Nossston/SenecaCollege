09f0c034aaac642e90ad1ca1770ad85e0d8cfae6
Nossston <37739219+Nossston@users.noreply.github.com>
2019-09-13 09:18:48 -0400

    Sept 13, 2019

---
+++ OOP345/w1_lab/process.h
@@ -0,0 +1,16 @@
+// Workshop 1 - Linkage, Storage Duration, Namespaces, and OS Interface
+// process.h
+// Chris Szalwinski - 2019/01/08
+// Cornel - 2019/09/06
+
+
+#ifndef process_h
+#define process_h
+
+
+
+namespace sdds
+{
+    void process(const char* str);
+}
+#endif /* process_h */
